99d792a46f418bb5d51b8b103d4e7176c653dce4
Richard Fox <richard.fox@ahref.co.uk>
2017-02-03 00:44:05 +0100

    Method handler exceptions

--- test/methodhandler.test.js
+++ test/methodhandler.test.js
@@ -1,9 +1,14 @@
 const methodHandler = require('../lib/methodhandler');
 const packets = require('../lib/packets');
+const Errors = require('../lib/errors');
 
 describe('method handler', () => {
+    let handler;
+    beforeEach(() => {
+        handler = new methodHandler.MethodHandlerManager();
+    });
+
     it('handles a registered method', () => {
-        const handler = new methodHandler.MethodHandlerManager();
         handler.addHandler('hello', method => { return Promise.resolve('hi')});
 
         return handler
@@ -11,5 +16,18 @@ describe('method handler', () => {
             .then(res => {
                 expect(res).to.equal('hi');
             })
+    });
+
+    it('throws an error if an undiscardable method has no handler', () => {
+        return handler
+            .handle(new packets.Method('hello', {foo: 'bar'}, false))
+            .catch(err => {
+                expect(err).to.be.an.instanceof(Errors.InteractiveError.UnknownMethodName);
+            })
+    });
+
+    it('does throws an error if a discardable method has no handler', () => {
+        return handler
+            .handle(new packets.Method('hello', {foo: 'bar'}, true));
     })
 });
