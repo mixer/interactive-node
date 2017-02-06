d9b01b849b742c8893bae218d579566e1b15ce46
Richard Fox <fox@xbox.com>
2017-02-06 15:01:51 +0100

    Start with 3 samples, cache delta result

--- src/ClockSync.ts
+++ src/ClockSync.ts
@@ -5,6 +5,9 @@ import { EventEmitter } from 'events';
 export class ClockSync extends EventEmitter {
     private syncTimer: NodeJS.Timer;
     private deltas: number[] = [];
+    private initialSampleSize: number = 3;
+
+    private cachedDelta: number = null;
     private syncFunc: () => Promise<number>;
 
     constructor(public syncInterval: number = 30 * 1000, private historyLength: number = 11) {
@@ -17,17 +20,23 @@ export class ClockSync extends EventEmitter {
         }
 
         this.syncFunc = syncFunc;
+        const samplePromises = [];
 
-        this.sync();
-        setInterval(
-            () => this.sync(),
-            this.syncInterval,
-        );
+        for(var i=0; i< this.initialSampleSize; i++) {
+            samplePromises.push(this.sync());
+        }
+
+        Promise.all(samplePromises).then(() => {
+            setInterval(
+                () => this.sync(),
+                this.syncInterval,
+            );
+        })
     }
 
-    private sync() {
+    private sync(): Promise<number> {
         const transmitTime = Date.now();
-        this.syncFunc().then(serverTime => this.processResponse(transmitTime, serverTime));
+        return this.syncFunc().then(serverTime => this.processResponse(transmitTime, serverTime));
     }
 
     public stop() {
@@ -37,7 +46,14 @@ export class ClockSync extends EventEmitter {
         this.deltas = [];
     }
 
-    public getDelta(): number {
+    public getDelta(forceCalculation?: boolean): number {
+        if (this.cachedDelta === null || forceCalculation) {
+            this.cachedDelta = this.calculateDelta();
+        }
+        return this.cachedDelta;
+    }
+
+    private calculateDelta(): number {
         if (this.deltas.length === 0) {
             return 0;
         }
@@ -56,20 +72,22 @@ export class ClockSync extends EventEmitter {
         }
     }
 
-    private processResponse(transmitTime: number, serverTime: number) {
+    private processResponse(transmitTime: number, serverTime: number): number {
         const recieveTime = Date.now();
         const rtt = recieveTime - transmitTime;
         const delta = serverTime - (rtt / 2) - transmitTime;
-        this.addDelta(delta);
+        return this.addDelta(delta);
     }
 
-    private addDelta(delta: number) {
+    private addDelta(delta: number): number {
         if (this.deltas.length === this.historyLength) {
             //Remove oldest
             this.deltas.shift();
         }
         //Add new one
         this.deltas.push(delta);
-        this.emit('delta', this.getDelta());
+
+        //Re-calculate delta with this number
+        return this.getDelta(true);
     }
 }
