55f17af5ab319abcd7c18cb3e7d287c13cca8f92
ProbablePrime <richard.fox@ahref.co.uk>
2017-03-21 15:48:36 +0100

    Fix exports

--- src/state/interfaces/controls/index.ts
+++ src/state/interfaces/controls/index.ts
@@ -1,7 +1,5 @@
-export * from './IGridConfig';
 export * from './IButton';
 export * from './IControl';
 export * from './IInput';
 export * from './IJoystick';
 export * from './IMeta';
-export * from './ITransaction';
