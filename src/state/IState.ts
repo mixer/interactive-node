99019ae232fcac7820d5ab4357588a306f37f95b
ProbablePrime <richard.fox@ahref.co.uk>
2017-04-12 16:56:05 +0200

    Create IClient

---
+++ src/state/IState.ts
@@ -0,0 +1,38 @@
+import { EventEmitter } from 'events';
+
+import { ClientType } from '../Client';
+import { IClient } from '../IClient';
+import { Method, Reply } from '../wire/packets';
+import { Group } from './Group';
+import { IScene, ISceneData } from './interfaces';
+import { IControl } from './interfaces/controls/IControl';
+import { IGroup } from './interfaces/IGroup';
+import { IParticipant } from './interfaces/IParticipant';
+
+export interface IState extends EventEmitter {
+    constructor(clientType: ClientType): this;
+
+    setClient(client: IClient): void;
+    processMethod(method: Method<any>): void | Reply;
+    synchronizeLocalTime(time: Date | number): Date;
+    synchronizeRemoteTime(time: Date | number): Date;
+
+    reset(): void;
+    updateScene(scene: ISceneData): void;
+    deleteScene(sceneID: string, reassignSceneID: string): void;
+
+    addScene(data: ISceneData): void;
+    updateGroup(group: IGroup): void;
+    deleteGroup(groupID: string, reassignGroupID: string): void;
+
+    addGroup(data: IGroup): Group;
+
+    getGroup(id: string): Group;
+    getScene(id: string): IScene;
+    getControl(id: string): IControl;
+
+    getParticipantByUserID(id: string): IParticipant;
+    getParticipantByUsername(name: string): IParticipant;
+    getParticipantBySessionID(id: string): IParticipant;
+
+}
