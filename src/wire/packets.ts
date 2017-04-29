9d2a70df419f70f2d49c3bdef64e3fe07591b828
ProbablePrime <richard.fox@ahref.co.uk>
2017-04-29 05:32:14 +0200

    Socket tweaks

--- src/wire/packets.ts
+++ src/wire/packets.ts
@@ -25,7 +25,7 @@ export enum PacketState {
 const maxInt32 = 0xFFFFFFFF;
 
 /**
- * A Packet is a wrapped Method that can be timed-out or canceled whilst it travels over the wire
+ * A Packet is a wrapped Method that can be timed-out or canceled whilst it travels over the wire.
  */
 export class Packet extends EventEmitter {
     private state: PacketState = PacketState.Pending;
@@ -93,35 +93,78 @@ export class Packet extends EventEmitter {
     }
 }
 
+/**
+ * A method represents a request from a client to call a method on the recipient.
+ * They can contain arguments which the recipient will use as arguments for the method.
+ *
+ * The Recipient can then reply with a result or an error indicating the method failed.
+ */
 export class Method<T> {
-    public type = 'method'; //tslint:disable-line
+    public readonly type = 'method'; //tslint:disable-line
 
     constructor(
+        /**
+         * The name of this method
+         */
         public method: string,
+
+        /**
+         * Params to be used as arguments for this method.
+         */
         public params: T,
+
+        /**
+         * If discard is set to true it indicates that this method is not expecting a reply.
+         *
+         * Recipients should however reply with an error if one is caused by this method.
+         */
         public discard: boolean = false,
+
+        /**
+         * A Unique id for each method sent.
+         */
         public id: number = Math.floor(Math.random() * maxInt32),
     ) {}
 
+    /**
+     * Creates a method instance from a JSON decoded socket message.
+     * @memberOf Method
+     */
     public static fromSocket(message: any): Method<IRawValues> {
         return  new Method(message.method, message.params, message.discard, message.id);
     }
 
+    /**
+     * Creates a reply for this method.
+     */
     public reply(result: IRawValues, error: InteractiveError.Base = null): Reply {
         return new Reply(this.id, result, error);
     }
 }
 
+/**
+ * A reply represents a recipients response to a corresponding method with the same id.
+ * It can contain a result or an error indicating that the method failed.
+ */
 export class Reply {
-    public type = 'reply'; //tslint:disable-line
+    public readonly type = 'reply'; //tslint:disable-line
     constructor(
+        /**
+         * A unique id for this reply, which must match the id of the method it is a reply for.
+         */
         public id: number,
+        /**
+         * The result of this method call.
+         */
         public result: IRawValues = null,
+        /**
+         * An error which if present indicates that a method call failed.
+         */
         public error: IInteractiveError = null,
     ) {}
 
     /**
-     * Constructs a Reply packet from raw values coming in from a socket
+     * Constructs a reply packet from raw values coming in from a socket.
      */
     public static fromSocket(message: any): Reply {
         const err: InteractiveError.Base = message.error ? InteractiveError.fromSocketMessage(message.error) : null;
@@ -129,7 +172,7 @@ export class Reply {
     }
 
     /**
-     * Construct a reply packet that indicates an error
+     * Construct a reply packet that indicates an error.
      */
     public static fromError(id: number, error: InteractiveError.Base): Reply {
         return new Reply(id, null, {
