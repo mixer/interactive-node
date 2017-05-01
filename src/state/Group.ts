7ed6af51047763ff559ff7f57709ad531c913129
Richard Fox <richard.fox@ahref.co.uk>
2017-05-01 18:41:16 +0200

    Final segment i think (#31)

--- src/state/Group.ts
+++ src/state/Group.ts
@@ -4,6 +4,9 @@ import { merge } from '../merge';
 import { IMeta } from './interfaces/controls';
 import { IGroup } from './interfaces/IGroup';
 
+/**
+ * A Group is a collection of participants.
+ */
 export class Group extends EventEmitter implements IGroup {
     public groupID: string;
     public sceneID: string;
@@ -17,6 +20,9 @@ export class Group extends EventEmitter implements IGroup {
 
     // TODO: group management, rather than read-only views
 
+    /**
+     * Updates this group with new data from the server.
+     */
     public update(data: IGroup) {
         merge(this, data);
         this.emit('updated', this);
