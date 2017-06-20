f6f4e67578460e446df9fff8987fbaee6beda472
Simon Schick <demwizzy@gmail.com>
2017-06-20 08:10:43 +0200

    remove module wrap and correct type info

--- src/constants.ts
+++ src/constants.ts
@@ -3,20 +3,20 @@ import { IGridLayout } from './state/interfaces/controls';
 /**
  * Offers constant information values to use in an application.
  */
-export module InteractiveConstants {
-    export const gridLayoutSizes: IGridLayout[] = <IGridLayout[]> Object.freeze([
-        {
-            size: 'large',
-            width: 80,
-            height: 20,
-        }, {
-            size: 'medium',
-            width: 45,
-            height: 25,
-        }, {
-            size: 'small',
-            width: 30,
-            height: 40,
-        },
-    ]);
-}
+export const gridLayoutSizes = Object.freeze(<IGridLayout[]>[
+    {
+        size: 'large',
+        width: 80,
+        height: 20,
+    },
+    {
+        size: 'medium',
+        width: 45,
+        height: 25,
+    },
+    {
+        size: 'small',
+        width: 30,
+        height: 40,
+    },
+]);
