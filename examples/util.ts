388d998531bf99948ae946e1062f758dd22b69d0
ProbablePrime <richard.fox@ahref.co.uk>
2017-08-07 20:56:36 +0200

    move getGridPlacement into utils

--- examples/util.ts
+++ examples/util.ts
@@ -1,5 +1,5 @@
-import { IButtonData, IControlData } from '../lib';
-export function makeControls(amount: number = 5, textGenerator: (i:number) => string):IControlData[] {
+import { IButtonData, IControlData, IGridPlacement } from '../lib';
+export function makeControls(amount: number = 5, textGenerator: (i: number) => string): IControlData[] {
     const controls: IButtonData[] = [];
     const size = 10;
     for (let i = 0; i < amount; i++) {
@@ -8,31 +8,35 @@ export function makeControls(amount: number = 5, textGenerator: (i:number) => st
             kind: 'button',
             text: textGenerator(i),
             cost: 1,
-            position: [
-                   {
-                       size: 'large',
-                       width: size,
-                       height: size / 2,
-                       x: i * size,
-                       y: 1,
-                   },
-                   {
-                       size: 'small',
-                       width: size,
-                       height: size / 2,
-                       x: i * size,
-                       y: 1,
-                   },
-                   {
-                       size: 'medium',
-                       width: size,
-                       height: size,
-                       x: i * size,
-                       y: 1,
-                   },
-               ],
-            },
-        );
+            position: getGridPlacement(i * size, 1, 10),
+        });
     }
     return controls;
+}
+
+// This makes grid placement objects dynamically, to be used for our controls below.
+export function getGridPlacement(x: number, y: number, size: number = 10): IGridPlacement[] {
+    return [
+        {
+            size: 'large',
+            width: size,
+            height: size,
+            x: x * size,
+            y: y * size,
+        },
+        {
+            size: 'medium',
+            width: size,
+            height: size,
+            x: x * size,
+            y: y * size,
+        },
+        {
+            size: 'small',
+            width: size,
+            height: size,
+            x: x * size,
+            y: y * size,
+        },
+    ];
 }
