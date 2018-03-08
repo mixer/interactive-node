f766cb79e212d7335327f8f101aa4c84c648fe91
hutchwhite <hutchjwhite@gmail.com>
2018-03-08 15:08:28 -0800

    feat(dropdown): add dropdown for crowd control

---
+++ src/state/controls/Dropdown.ts
@@ -0,0 +1,9 @@
+import { IControl, IControlData } from '../interfaces/controls/IControl';
+import { Control } from './Control';
+
+/**
+ * @TODO: This is a placeholder for now.  Currently used for Crowd Control
+ */
+export class Dropdown extends Control<IControlData> implements IControl {
+
+}
